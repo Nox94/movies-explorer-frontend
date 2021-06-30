4a7da9640d3bf2e3f1d93c68f5478ce6612d8aba
Евгения <69312077+Nox94@users.noreply.github.com>
2021-06-30 13:20:19 +0300

    добавляет открытие ссылок в новом окне

--- src/components/Main/NavTab/NavTab.js
+++ src/components/Main/NavTab/NavTab.js
@@ -5,13 +5,13 @@ function NavTab() {
         <section className="navTab">
             <ul className="navTab__list">
                 <li className="navTab__list-unit">
-                    <a className="navTab__link link" href="#aboutProject">О проекте</a>
+                    <a className="navTab__link link" href="#aboutProject" target="_self">О проекте</a>
                 </li>
                 <li className="navTab__list-unit">
-                    <a className="navTab__link link" href="#techs">Технологии</a>
+                    <a className="navTab__link link" href="#techs" target="_self">Технологии</a>
                 </li>
                 <li className="navTab__list-unit">
-                    <a className="navTab__link link" href="#student">Студент</a>
+                    <a className="navTab__link link" href="#student" target="_self">Студент</a>
                 </li>
             </ul>
         </section>
