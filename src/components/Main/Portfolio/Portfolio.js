8f540fc6233f942c49ee5f52d58cfd3791df2e3c
Евгения <69312077+Nox94@users.noreply.github.com>
2021-06-30 13:33:23 +0300

    добавляет ссылки в портфолио

--- src/components/Main/Portfolio/Portfolio.js
+++ src/components/Main/Portfolio/Portfolio.js
@@ -7,21 +7,21 @@ function Portfolio() {
             <h2 className="portfolio__heading">Портфолио</h2>
             <ul className="portfolio__list">
                 <li className="portfolio__list-item">
-                    <a className="portfolio__link link" href="#">
+                    <a className="portfolio__link link" href="https://github.com/Nox94/how-to-learn">
                         Статичный сайт
                         <span className="portfolio__link-arrow">&#129125;</span>
                     </a>
                 </li>
                 <hr className="portfolio__line" noshade="true" size="1" color="#000000"/>
                 <li className="portfolio__list-item">
-                    <a className="portfolio__link link" href="#">
+                    <a className="portfolio__link link" href="https://nox94.github.io/russian-travel/">
                         Адаптивный сайт
                         <span className="portfolio__link-arrow">&#129125;</span>
                     </a>
                 </li>
                 <hr className="portfolio__line" noshade="true" size="1" color="#000000"/>
                 <li className="portfolio__list-item">
-                    <a className="portfolio__link link" href="#">
+                    <a className="portfolio__link link" href="https://github.com/Nox94/react-mesto-api-full">
                         Одностраничное приложение
                         <span className="portfolio__link-arrow">&#129125;</span>
                     </a>
